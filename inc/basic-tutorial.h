205260b50e9f285fb659c23ed6caef3ba95ce378
mengqingge <mengqingge@neusoft.com>
2024-01-02 17:23:26 +0800

    add tutorial-4

--- inc/basic-tutorial.h
+++ inc/basic-tutorial.h
@@ -3,3 +3,4 @@
 int tutorial_main_1 (int argc, char *argv[]);
 int tutorial_main_2 (int argc, char *argv[]);
 int tutorial_main_3 (int argc, char *argv[]);
+int tutorial_main_4 (int argc, char *argv[]);
