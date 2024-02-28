dd3859a77b58a45ddc5fd10f32ee6af9d5b40a8a
mengqingge <mengqingge@neusoft.com>
2024-02-28 16:34:50 +0900

    add

--- inc/basic-tutorial.h
+++ inc/basic-tutorial.h
@@ -1,9 +1,10 @@
 #include <gst/gst.h>
 #include <stdio.h>
-int32_t tutorial_main_1 (int32_t argc, char *argv[]);
-int32_t tutorial_main_2 (int32_t argc, char *argv[]);
-int32_t tutorial_main_3 (int32_t argc, char *argv[]);
-int32_t tutorial_main_4 (int32_t argc, char *argv[]);
-int32_t tutorial_main_5 (int32_t argc, char *argv[]);
-int32_t tutorial_main_6 (int32_t argc, char *argv[]);
-int32_t tutorial_main_7 (int32_t argc, char *argv[]);
+int32_t tutorial_main_1(int32_t argc, char *argv[]);
+int32_t tutorial_main_2(int32_t argc, char *argv[]);
+int32_t tutorial_main_3(int32_t argc, char *argv[]);
+int32_t tutorial_main_4(int32_t argc, char *argv[]);
+int32_t tutorial_main_5(int32_t argc, char *argv[]);
+int32_t tutorial_main_6(int32_t argc, char *argv[]);
+int32_t tutorial_main_7(int32_t argc, char *argv[]);
+int32_t tutorial_main_12(int32_t argc, char *argv[]);
--- main.cpp
+++ main.cpp
@@ -7,15 +7,15 @@ int32_t main(int32_t argc, char *argv[])
 {
   InFunLOG();
 #if 1
-  // int32_t ret = tutorial_main_7(argc,argv);
+  int32_t ret = tutorial_main_12(argc,argv);
 #endif
 
 #if 1
   // Sample sample {};
   // int32_t ret = sample.tutorial_main_4(argc,argv);
 #endif 
-  int32_t ret = build_pipeline(argc,argv);
-  printf("ret = %d\n",ret);
+  // int32_t ret = build_pipeline(argc,argv);
+  // printf("ret = %d\n",ret);
   return ret;
 
 }
